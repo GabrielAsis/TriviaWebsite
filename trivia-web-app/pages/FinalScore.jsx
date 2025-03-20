1e0418ca28249e8a38be1425230c3c82ac0574ae
GabrielAsis <113765432+GabrielAsis@users.noreply.github.com>
2025-03-20 16:13:56 +0400

    Final Score Screen added
    Final score pops up after answering all questions. Will add more design and modification on these aspects later on.
    
    API Testing Now Completed

--- trivia-web-app/pages/FinalScore.jsx
+++ trivia-web-app/pages/FinalScore.jsx
@@ -1,9 +1,27 @@
+import { Box, Button, Typography} from "@mui/material"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom";
+import { handleAmountChange, handleScoreChange } from "../redux/actions";
+ 
 const FinalScore = () => {
+  const dispatch = useDispatch();
+  const { score } = useSelector(state => state);
+  const navigate = useNavigate();
+
+  const handleBackToSettings = () => {
+    dispatch(handleScoreChange(0));
+    dispatch(handleAmountChange(50));
+    navigate("/");
+  }
+
   return (
-    <div>
-      finalScreen
-    </div>
+    <Box mt={30}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>
+        Your Final Score: {score}
+      </Typography>
+      <Button onClick={handleBackToSettings} variant="outlined">Back to Home</Button>
+    </Box>
   )
-}
+};
 
 export default FinalScore
