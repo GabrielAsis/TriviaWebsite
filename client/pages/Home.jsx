0629b355a2181bff94dba9b737dc927eaf777c67
GabrielAsis <asisgab24@gmail.com>
2025-04-12 22:37:06 +0400

    Setting Up Frontend
    - Installing Shadcn for UI
    - Setting up colors, font family, and font sizes

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>H1</h1>
+      <h2>H2</h2>
+      <h2>H2</h2>
+      <h3>H3</h3>
+      <h4>H4</h4>
+      <p>Lorem Ipsum </p>
+      
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </div>
+
   )
 }
