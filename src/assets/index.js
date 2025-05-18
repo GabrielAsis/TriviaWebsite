c2e02baf7b134c8ff023978b9d47516e66db1c7e
GabrielAsis <113765432+GabrielAsis@users.noreply.github.com>
2025-05-18 13:30:00 +0400

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -14,7 +14,7 @@ import one from "./1.svg"
 import two from "./2.svg"
 import three from "./3.svg"
 import whiteBlitz from "./White Blitz Mode.svg"
-import whiteStrike from "./White Strike mode.svg";
+import whiteStrike from "./White Strike Mode.svg";
 import whiteEndless from "./White Endless Mode.svg";
 
 // misc
